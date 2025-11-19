3eb040898d0f4e97209d71e8f65b774284c0819c
Hasan Rahic <haso.raha10@gmail.com>
2025-11-19 10:55:14 +0100

    Treci susret (zavrseni zadaci)

--- src/app/components/zadatak/zadatak.component.ts
+++ src/app/components/zadatak/zadatak.component.ts
@@ -106,11 +106,44 @@ export class ZadatakComponent {
     { code: "3214", name: "Coca Cola", price: 100, quantity: 20, discount: false, description: "Coca Cola je odlicna", image: "https://www.1001spirits.com/tuotekuvat/1200x1200/Coca%20Cola%20Classic%2024x0%2C33%20l.png" },
     { code: "3224", name: "Coca Cola Zero", price: 600, quantity: 15, discount: false, description: "Coca Cola je odlicna i jos je zero bas 0 secera ima kako kul", image: "https://shop.stridon.hr/wp-content/uploads/2023/03/Coca-Cola-zero-sugar-330mL.jpg" },
     { code: "3114", name: "Sprite", price: 1500, quantity: 40, discount: false, description: "Ovo", image: "https://chamberswineandliquor.com/wp-content/uploads/2021/12/4913207.jpg" },
-    { code: "3211", name: "Pepsi", price: 3000, quantity: 4, discount: true, description: "Pepsi je bolji od kole", image: "https://www.knjaznatura.co.rs/files/watermark/files/files/thumbs_w/pepsi-BLUE-TITAN-0_33L-SRB_w_800_800px.jpg" },
+    { code: "3211", name: "Pepsi", price: 3000, quantity: 4, discount: true, description: "Pepsi je bolji od kole", image: "https://www.knjaznatura.co.rs/files/watermark/files/files/thumbs_w/pepsi-BLUE-TITAN-0_33L-SRB_w_800_800px.jpg" }
   ];
 
-  vrijednost: number = 0;
-  saberi(n: any, m: any){
-    this.vrijednost = parseFloat(n) + parseFloat(m);
+  broj1: number =  0;
+  broj2: number = 0;
+  suma: number = 0;
+  Input1(event: any){
+    this.broj1 = Number(event.target.value);
+    this.saberi();
   }
+  Input2(event:any){
+    this.broj2 = Number(event.target.value);
+    this.saberi();
+  }
+  saberi(){
+    this.suma = this.broj1 + this.broj2;
+  }
+
+
+  korisnici: any[] = [
+    {code: 123, name: "Korisnik"},
+    {code: 125, name: "Neki korisnik"},
+    {code: 122, name: "abc korisnik"},
+    {code: 121, name: "Haso"},
+    {code: 127, name: "Dara"},
+    {code: 124, name: "Ime"},
+    {code: 120, name: "Neki ime"},
+    {code: 129, name: "Korisnik novi"},
+    {code: 128, name: "Darius"},
+    {code: 126, name: "Haso"}
+  ];
+  filtriraniKorisnici: any[] = [];
+  filtriranje(event: any){
+    var tekst = event.target.value.toLowerCase();
+    for (let i = 0; i < this.korisnici.length; i++) {
+      this.filtriraniKorisnici = this.korisnici.filter(k => k.name.toLowerCase().includes(tekst));
+
+    }
+  }
+
 }
