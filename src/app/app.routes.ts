c49c4b0c205bf9322b92204f28ff18f52dcf9b2e
HasanRahic <haso.raha10@gmail.com>
2025-12-02 16:31:38 +0100

    commit za moga darisa susret 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'products', component: ProductListComponent},
     {path: 'about-us', component: AboutUsComponent},
+    {path: '**', component: NotFoundComponent},
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,47 @@
-import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { DatePipe, NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   today: any = new Date;
+  productName: string = "Coca cola";
+  selectedType: number=1;
+  selectedTypeName: string = "";
+  productTypeList: any[] = [
+    {id:1, name:"Cokolada"},
+    {id:2, name: "Pice"},
+    {id:3, name: "Jelo"},
+    {id:4, name: "Bombone"},
+  ];
+
+
+  ngOnInit(): void {
+    this.promijenitProductType();
+  }
+
+  changeProductName(){
+    console.log(this.productName);
+  }
+
+
+  promijeniVrijednost(){
+    console.log(this.selectedType);
+  }
+
+
+  promijenitProductType(){
+    for (let index = 0; index < this.productTypeList.length; index++) {
+      if(this.selectedType==this.productTypeList[index].id){
+        this.selectedTypeName = this.productTypeList[index].name;
+      }
+      
+    }
+  }
 }
